88aad7c155a0c7da3c61859faf1bfa14b38e801c
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2024-04-26 17:36:07 -0300

    adcionado conexão com cliente

--- projeto_bd/src/main/java/com/projeto/bd/DAO/ClienteDAO.java
+++ projeto_bd/src/main/java/com/projeto/bd/DAO/ClienteDAO.java
@@ -3,25 +3,26 @@ package com.projeto.bd.DAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import com.projeto.bd.models.ChefeDeDepartamento;
+import com.projeto.bd.models.Cliente;
 
-public class ChefeDeDepartamentoDAO extends ConnectionDAO{
+public class ClienteDAO extends ConnectionDAO{
     
     //DAO - Data Access Object
     private boolean sucesso = false; //Para saber se funcionou
 
     //INSERT
-    public boolean insertChefeDeDepartamento(ChefeDeDepartamento ChefeDeDepartamento) {
+    public boolean insertCliente(Cliente Cliente) {
 
         connectToDB();
 
-        String sql = "INSERT INTO chefe_de_departamento (nome, matricula, email, telefone) values(?,?,?,?)";
+        String sql = "INSERT INTO cliente (nome, email, telefone, endereco, data_de_nascimento) values(?,?,?,?,?)";
         try {
             pst = con.prepareStatement(sql);
-            pst.setString(1,ChefeDeDepartamento.getNome());
-            pst.setInt(2, ChefeDeDepartamento.getMatricula());
-            pst.setString(3, ChefeDeDepartamento.getEmail());
-            pst.setString(4, ChefeDeDepartamento.getTelefone());
+            pst.setString(1,Cliente.getNome());
+            pst.setString(2, Cliente.getEmail());
+            pst.setString(3, Cliente.getTelefone());
+            pst.setString(4, Cliente.getEndereco());
+            pst.setString(5, Cliente.getData_de_nascimento());
             pst.execute();
             sucesso = true;
         } catch (SQLException exc) {
@@ -39,16 +40,17 @@ public class ChefeDeDepartamentoDAO extends ConnectionDAO{
     }
 
     //UPDATE
-    public boolean updateChefeDeDepartamentoNome(String nome, int matricula, String email, String telefone, String novoNome) {
+    public boolean updateClienteNome(String nome, String email, String telefone, String endereco, String data_de_nascimento, String novoNome) {
         connectToDB();
-        String sql = "UPDATE chefe_de_departamento SET nome=?, matricula=?, email=?, telefone=? where nome=?";
+        String sql = "UPDATE cliente SET nome=?, email=?, telefone=?, endereco=?, data_de_nascimento=? where nome=?";
         try {
             pst = con.prepareStatement(sql);
             pst.setString(1, novoNome);
-            pst.setInt(2, matricula);
-            pst.setString(3, email);
-            pst.setString(4, telefone);
-            pst.setString(5, nome);
+            pst.setString(2, email);
+            pst.setString(3, telefone);
+            pst.setString(4, endereco);
+            pst.setString(5, data_de_nascimento);
+            pst.setString(6, nome);
             pst.execute();
             sucesso = true;
         } catch (SQLException ex) {
@@ -66,9 +68,9 @@ public class ChefeDeDepartamentoDAO extends ConnectionDAO{
     }
 
     //DELETE
-    public boolean deleteChefeDeDepartamento(String nome) {
+    public boolean deleteCliente(String nome) {
         connectToDB();
-        String sql = "DELETE FROM chefe_de_departamento where nome=?";
+        String sql = "DELETE FROM cliente where nome=?";
         try {
             pst = con.prepareStatement(sql);
             pst.setString(1, nome);
@@ -89,10 +91,10 @@ public class ChefeDeDepartamentoDAO extends ConnectionDAO{
     }
 
     //SELECT
-    public ArrayList<ChefeDeDepartamento> selectChefeDeDepartamento() {
-        ArrayList<ChefeDeDepartamento> ChefeDeDepartamentos = new ArrayList<>();
+    public ArrayList<Cliente> selectCliente() {
+        ArrayList<Cliente> Clientes = new ArrayList<>();
         connectToDB();
-        String sql = "SELECT * FROM chefe_de_departamento";
+        String sql = "SELECT * FROM cliente";
 
         try {
             st = con.createStatement();
@@ -102,15 +104,16 @@ public class ChefeDeDepartamentoDAO extends ConnectionDAO{
 
             while (rs.next()) {
 
-                ChefeDeDepartamento ChefeDeDepartamentoAux = new ChefeDeDepartamento(rs.getString("nome"), rs.getInt("matricula"), rs.getString("email"), rs.getString("telefone"));
+                Cliente ClienteAux = new Cliente(rs.getString("nome"), rs.getString("email"), rs.getString("telefone"), rs.getString("endereco"),rs.getString("data_de_nascimento"));
 
-                System.out.println("nome = " + ChefeDeDepartamentoAux.getNome());
-                System.out.println("matricula = " + ChefeDeDepartamentoAux.getMatricula());
-                System.out.println("email = " + ChefeDeDepartamentoAux.getEmail());
-                System.out.println("telefone = " + ChefeDeDepartamentoAux.getTelefone());
+                System.out.println("nome = " + ClienteAux.getNome());
+                System.out.println("email = " + ClienteAux.getEmail());
+                System.out.println("telefone = " + ClienteAux.getTelefone());
+                System.out.println("endereco = " + ClienteAux.getEndereco());
+                System.out.println("data de nascimento = " + ClienteAux.getData_de_nascimento());
                 System.out.println("--------------------------------");
 
-                ChefeDeDepartamentos.add(ChefeDeDepartamentoAux);
+                Clientes.add(ClienteAux);
             }
             sucesso = true;
         } catch (SQLException e) {
@@ -124,7 +127,7 @@ public class ChefeDeDepartamentoDAO extends ConnectionDAO{
                 System.out.println("Erro: " + e.getMessage());
             }
         }
-        return ChefeDeDepartamentos;
+        return Clientes;
     }
 
 }
